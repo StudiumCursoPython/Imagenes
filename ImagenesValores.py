eeca676950174af9f4236d29ccacc1e03b2fe10a
informaticosonline <109153512+informaticosonline@users.noreply.github.com>
2023-11-23 18:47:36 +0100

    Repasos generales

--- ImagenesValores.py
+++ ImagenesValores.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
-ancho = input("Dame el ancho: ")
-alto = input("Dame el alto: ")
+ancho = input ("Dame el ancho: ")
+alto = input ("Dame el alto: ")
 
 def redimensionar_imagen(ruta_entrada, ruta_salida, tamaño):
     with Image.open(ruta_entrada) as img:
@@ -16,6 +16,6 @@ ruta_entrada = r'C:/Users/Pepe/Documents/CursoFormacionPython/Imagenes/ImgCurso.
 ruta_salida = r'C:/Users/Pepe/Documents/CursoFormacionPython/Imagenes/redimensionadaEjercicio2.png'
 
 # Nuevo tamaño en píxeles
-nuevo_tamaño = (ancho, alto)  
+nuevo_tamaño = (int(ancho), int(alto))  
 
 redimensionar_imagen(ruta_entrada, ruta_salida, nuevo_tamaño)
